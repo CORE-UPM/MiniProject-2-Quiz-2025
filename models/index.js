5ef3ceec7c690e124aad93ed0d36ee1e8cc7303f
Santiago Pavon <santiago.pavon@upm.es>
2022-01-17 13:30:58 +0100

    Quiz with Attachment.

--- models/index.js
+++ models/index.js
@@ -9,5 +9,10 @@ const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
 const sequelize = new Sequelize(url);
 
 const Quiz = require('./quiz')(sequelize);
+const Attachment = require('./attachment')(sequelize);
+
+// Relation 1-to-1 between Quiz and Attachment
+Quiz.hasOne(Attachment, {as: 'attachment', foreignKey: 'quizId'});
+Attachment.belongsTo(Quiz, {as: 'quiz', foreignKey: 'quizId'});
 
 module.exports = sequelize;
